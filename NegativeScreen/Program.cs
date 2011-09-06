753bd4c367a2854f0623099d56679719a9764c75
yaurthek <devnull@localhost>
2011-09-06 20:36:03 +0200

    check si l'application est lancée, et s'auto détruit de façon à n'avoir qu'une seul instance. cela permet d'éviter un crash tout moche du à l'enregistrement des hotkeys en double...

--- NegativeScreen/Program.cs
+++ NegativeScreen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace NegativeScreen
 {
@@ -6,7 +7,30 @@ namespace NegativeScreen
 	{
 		static void Main(string[] args)
 		{
+			if (IsAnotherInstanceAlreadyRunning())
+			{
+				//show message and exit
+				System.Windows.Forms.MessageBox.Show("The application is already running !", "Warning", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation, System.Windows.Forms.MessageBoxDefaultButton.Button1);
+				return;
+			}
+
 			NegativeOverlay overlay = new NegativeOverlay();
 		}
+
+		private static bool IsAnotherInstanceAlreadyRunning()
+		{
+			Process me = Process.GetCurrentProcess();
+			Process[] processesWithSameName = Process.GetProcessesByName(me.ProcessName);
+			foreach (Process process in processesWithSameName)
+			{
+				// same process name, was started from the same file name and location.
+				if (process.Id != me.Id && process.MainModule.FileName == me.MainModule.FileName)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
 	}
 }
