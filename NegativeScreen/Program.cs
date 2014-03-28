ed2a5016301fd39977cfc0bead333793206e17e2
Melvyn <melvyn.laily@gmail.com>
2014-03-28 11:52:57 +0100

    updated the warning when starting without aero

--- NegativeScreen/Program.cs
+++ NegativeScreen/Program.cs
@@ -50,7 +50,7 @@ http://x2a.yt?negativescreen", "Warning", System.Windows.Forms.MessageBoxButtons
 			//check whether aero is enabled
 			if (Configuration.Current.ShowAeroWarning && !NativeMethods.DwmIsCompositionEnabled())
 			{
-				var result = System.Windows.Forms.MessageBox.Show("Windows Aero should be enabled for this program to work properly!\nOtherwise, you may experience bad performances.", "Warning", System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Information, System.Windows.Forms.MessageBoxDefaultButton.Button1);
+				var result = System.Windows.Forms.MessageBox.Show("Windows Aero must be enabled for this program to work properly!", "Warning", System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Information, System.Windows.Forms.MessageBoxDefaultButton.Button1);
 				if (result != System.Windows.Forms.DialogResult.OK)
 				{
 					return;
